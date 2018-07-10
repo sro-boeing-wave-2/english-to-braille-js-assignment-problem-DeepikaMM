300a027c1590aa245a21655274cdccdbb0677eba
Deepika Mohan <deepikamm12@gmail.com>
2018-07-10 11:35:04 +0530

    updated

--- index.js
+++ index.js
@@ -5,3 +5,16 @@
  * in english language to that in the equivalent braille
  * code.
  */
+import englishToBrailleLiteralSet from './english-to-braille.js';
+
+let res = '';
+let english = '';
+const myMap = new Map(englishToBrailleLiteralSet);
+function displaybraille() {
+  english = document.getElementById('sourceLangText').value;
+  for (let i = 0; i < english.length; i += 1) {
+    res += myMap.get(english[i]);
+  }
+  document.getElementById('targetLangText').innerHTML = res;
+}
+document.getElementById('btnConvertEnglishToBraille').addEventListener('click', displaybraille);
